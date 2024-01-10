9f9d9271164c94a0095976fdc2b8e356960b3639
Uday <udaysharma4600@gmail.com>
2024-01-10 15:30:00 +0530

    JavaScript Asyn=>Callback,Promises

---
+++ JAVASCRIPT/33_JavaScript_Async/CallBack hell and Pyramid of Doom/CallBack_Funcition/CallBack.js
@@ -0,0 +1,40 @@
+// Syncronous programming
+// const prompt = require("prompt-sync")();
+// let a = prompt("What is your name?");
+// let b = prompt("What is your age?");
+// let c = prompt("What is your favorite color?");
+// console.log(a + " is " + b + " years old and has " + c + " favorite color.");
+
+// // Asynchronous programming
+// console.log("start"); // Compile First
+// setTimeout(()=>{
+//   console.log("The Asynchronus Code in JavaScrip"); // Run After 3seconds
+// },3000)
+// console.log("End");// Compile First
+
+// Loading a Script
+/*
+function loadScript(src){
+    let script = document.createElement("script");
+    script.src = src;
+    document.body.appendChild(script);
+}
+
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js")
+*/
+
+
+// Callbacks with no-argumnet
+function loadScript(src,callback) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = function() {
+    console.log("Loaded script with SRC: " + src)
+    callback();   // callback the method
+  }
+  document.body.appendChild(script);
+}
+function hI(){
+  alert("Hello");
+}
+loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",hI) ; // giving the method as the argument
---
+++ JAVASCRIPT/33_JavaScript_Async/CallBack hell and Pyramid of Doom/Handling_Error_in_CallBack_function/Handling_Error_in_CallBack_function.js
@@ -0,0 +1,32 @@
+function loadScript(src,callback){
+    let script = document.createElement('script');
+     script.src=src;
+     script.onload = ()=>{
+        console.log("Loaded script with SRC: " + src)
+        callback(null, src);
+     }
+     script.onerror= ()=>{
+        console.log("Error loading script with SRC: " + src);
+        callback(new Error("Src got some error"))
+     }
+     document.body.appendChild(script);
+}
+function hello(error, src) {
+    if (error) {
+      console.log(error)
+      return
+    }
+    alert('Hello World!' + src);
+  }
+  
+  function goodmorning(error, src) {
+    if (error) {
+      console.log(error)
+      sendEmergencyMessageToCeo();
+      return
+    }
+    alert('Good morning' + src);
+  }
+  
+  loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",goodmorning) ; // giving the method as the argument
+  
